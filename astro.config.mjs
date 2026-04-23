7b1e7e82cabc8aba1a44127d1e141046309a847e
Luke <lukewzcannon@gmail.com>
2026-04-23 10:13:13 +0800

    Remove node_modules (sue me)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://swordsalesman.github.io",
+	// site: "https://swordsalesman.github.io",
+	site: "https://lukecannon.dev",
 	// Needed for GitHub pages. Remove for local dev
-    base: "/portfolio2",
+	// base: "/portfolio2",
 });
